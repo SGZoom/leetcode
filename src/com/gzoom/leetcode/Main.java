c9adb5731d80067825f913d76aec1502736034e0
SGZoom <g_zoom@163.com>
2019-03-26 17:50:56 +0800

    解决反转连表

--- src/com/gzoom/leetcode/Main.java
+++ src/com/gzoom/leetcode/Main.java
@@ -26,43 +26,78 @@ public class Main {
      * 1 ≤ m ≤ n ≤ 链表长度。
      *
      * 出现的问题：
-     * 1、超过时间限制，不知道为什么
+     * 1、超过时间限制，不知道为什么:最后没有处理好，死循环了
+     * 2、出现的极端情况：列表只有一个、列表从头开始倒序（你就不能用头）、m==n（那就不用处理了）
      * */
     public ListNode reverseBetween(ListNode head, int m, int n) {
         ListNode mHead = head;
         ListNode last = null;
+        if (head == null || head.next == null) {
+            return head;
+        }
+        if (m == n) {
+            return head;
+        }
         int i = 1;
         List<ListNode> handle = new ArrayList<>();
+        ListNode listTop = null;
+        ListNode oriTop = null;
+        ListNode bottomNode = null;
+
         while (head != null) {
-            if(handle.contains(head)) {
+            if (handle.contains(head)) {
                 break;
             }
             handle.add(head);
-            if (i >= m && i <= n) {
+            if (i == m) {
+                listTop = head;
+                oriTop = last;
+                last = head;
+                head = head.next;
+                listTop.next = null;
+
+            } else if (i == n) {
                 ListNode cur = head;
                 head = head.next;
                 cur.next = last;
                 last = head;
+                if (oriTop != null) {
+                    oriTop.next = cur;
+                }
+                listTop.next = head;
+                bottomNode = cur;
+            } else if (i > m && i < n) {
+                ListNode cur = head;
+                head = head.next;
+                cur.next = last;
+                last = cur;
             } else {
                 last = head;
                 head = head.next;
             }
             i++;
         }
+        if (m == 1) {
+           return bottomNode;
+        }
         return mHead;
     }
 
     public void test_reverseBetween() {
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
+        ListNode node1 = new ListNode(3);
+        ListNode node2 = new ListNode(5);
         ListNode node3 = new ListNode(3);
         ListNode node4 = new ListNode(4);
         ListNode node5 = new ListNode(5);
 
         node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        reverseBetween(node1,2,4);
+//        node2.next = node3;
+//        node3.next = node4;
+//        node4.next = node5;
+        node1 = reverseBetween(node1,1,2);
+        while (node1 != null) {
+            System.out.println(node1.val);
+            node1 = node1.next;
+        }
     }
 }
